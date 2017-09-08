dab745a6289e37f6a2775094aec8de73602dca60
Jonathan Lee <jonathanleeee@gmail.com>
2017-09-08 14:26:05 -0700

    working on it

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// TODO
+// var Apple = () => (
+//     	<li>Apple</li>
+// );
+
+
+// var Orange = () => (
+//     	<li>Orange</li>
+// );
+
+
+var GroceryListItem = (props) => (
+    <li>props.groceryTodo[0]</li>
+    <li>props.groceryTodo[1]</li>
+);
+
+var App = () => (
+  <div>
+    <h2>groceryList</h2>
+    <ul>
+			<GroceryListItem groceryTodo={['Apple', 'Orange']}/>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
