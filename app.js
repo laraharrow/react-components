3229fc3ff61270db3c8aec7a2e334a634237866d
Hack Reactor Student <student@hackreactor.com>
2017-09-08 18:05:50 -0700

    updated the bind to happend on the constructor so it just happends once

--- app.js
+++ app.js
@@ -1,25 +1,58 @@
-// var Apple = () => (
-//     	<li>Apple</li>
-// );
+// make a stateless component into a class component
+class GroceryListItem extends React.Component {
+  
+  constructor(props) {
+    super(props); 
+    
+    this.state = {
+      done: false
+    }; 
+    this.onListItemHover = this.onListItemHover.bind(this);
+  }  
+  
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    }, function () {console.log('On list item hover invoked!')});
+  }
+  
+  render() {
+    
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+      //textDecoration: this.state.done ? 'line-through' : 'none'
+    };  
+    
+    return (
+      <li style={style} onMouseEnter={this.onListItemHover}>{this.props.item}</li>  
+    )
+  } 
+}
 
 
-// var Orange = () => (
-//     	<li>Orange</li>
-// );
+//create a second component to be rendered by component App
+var GroceryList = (props) => (
+  <ul>
+    {props.list.map(item => 
+      <GroceryListItem item={item}/>   
+    )}
+  </ul>  
+)
 
-
-var GroceryListItem = (props) => (
-    <li>props.groceryTodo[0]</li>
-    <li>props.groceryTodo[1]</li>
-);
-
+// creat a component
 var App = () => (
   <div>
-    <h2>groceryList</h2>
-    <ul>
-			<GroceryListItem groceryTodo={['Apple', 'Orange']}/>
-    </ul>
+    <h2>Grocery List</h2>
+    <GroceryList list={['avocado', 'cheese', 'milk']}/>  
   </div>
 );
 
+// render it to the DOM
 ReactDOM.render(<App />, document.getElementById("app"));
+
+
+
+
+
+
+
